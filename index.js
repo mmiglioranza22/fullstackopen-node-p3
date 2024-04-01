effac4127f41e9a519146b9b83e27ed230548966
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-04-01 16:47:53 -0300

    3.7 - 3.8

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router");
+const morgan = require("morgan");
 const app = express();
 const PORT = 3001;
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length] :body")
+);
 app.use("/api", router);
 
 app.listen(PORT);
